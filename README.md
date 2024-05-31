b068c73abd27d3465012c8e6a6373d7795f27eb3
Piotr Dziurny <aisujd1@gmail.com>
2024-05-31 13:54:43 +0200

    scraped data

---
+++ README.md
@@ -0,0 +1,15 @@
+## Simple scraper for ogloszenia.trojmiasto.pl
+
+The scraped data consists of apartments from the "rynek pierwotny" and "rynek wtórny" categories.
+
+### Scraped data
+
+- **url**: URL of the listing
+- **title**: title of the listing
+- **price**: price of the apartment
+- **rooms**: number of rooms in the apartment
+- **floor**: floor on which the apartment is located
+- **year**: year the building was constructed
+- **price per square meters**: price per square meter of the apartment
+- **square meters**: total area of the apartment in square meters
+- **address**: address of the apartment
---
+++ ogloszenia_trojmiasto/spiders/ogloszenia.py
@@ -0,0 +1,70 @@
+import scrapy
+
+class OgloszeniaSpider(scrapy.Spider):
+    name = "ogloszenia"
+    allowed_domains = ["ogloszenia.trojmiasto.pl"]
+    start_urls = [
+        "https://ogloszenia.trojmiasto.pl/nieruchomosci-rynek-wtorny/mieszkanie/",
+        "https://ogloszenia.trojmiasto.pl/nieruchomosci-rynek-pierwotny/mieszkanie/"
+    ]
+
+
+    def parse(self, response):
+        listings = response.css('div.list__item') # main class showing all listings
+        for listing in listings:
+            relative_url = listing.css("h2.list__item__content__title a::attr(href)").get() # current site
+            listing_url = response.urljoin(relative_url)
+            yield response.follow(listing_url, callback = self.parse_subsite) # entering subsite
+
+        next_page = response.css("div.pages__controls.pages__controls--right a::attr(href)").get()
+        if next_page is not None:
+            next_page_url = response.urljoin(next_page)
+            yield response.follow(next_page_url, callback = self.parse)
+
+
+    def parse_subsite(self, response):
+        try:
+            title = response.css("h1.xogIndex__title::text").get()
+        except:
+            title = None
+        try:
+            price = response.css(".xogParams p::text").get()[:-2].strip()
+        except:
+            price = None
+        try:
+            rooms = response.css("span:contains('Liczba pokoi') + span::text").get().strip()
+        except:
+            rooms = None
+        try:
+            floor = response.css("span:contains('Piętro') + span::text").get().strip()
+        except:
+            floor = None
+        try:
+            year = response.css("span:contains('Rok budowy') + span::text").get().strip()
+        except:
+            year = None
+        try:
+            price_per_sqr_meter = response.css("span:contains('Cena za m') + span::text").get().strip()
+        except:
+            price_per_sqr_meter = None
+        try:
+            square_meters = response.css("span:contains('Powierzchnia') + span::text").get().strip()
+        except:
+            square_meters = None
+        try:
+            address = response.css("i.trm.trm-location + span::text").getall()
+        except:
+            address = None
+
+        yield {
+            "url" : response.url,
+            "title" : title,
+            "price" : price,
+            "rooms" : rooms,
+            "floor" : floor,
+            "year" : year,
+            "price_per_sqr_meter" : price_per_sqr_meter,
+            "square_meters" : square_meters,
+            "address" : address,
+            ### more features?
+        }
